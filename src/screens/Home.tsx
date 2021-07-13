0f759c40ea95e46e18fdfbacd42b3d444c4e6f4c
Diego Emilio Fuentes <diego.fuentes@uc.cl>
2021-07-13 17:04:56 -0400

    initial proyect install icons and navigation

---
+++ src/screens/Home.tsx
@@ -0,0 +1,14 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+export const Home = () => {
+    return (
+        <NavigationContainer>
+            <Text>Home</Text>
+        </NavigationContainer>
+    )
+}
+
+export default Home;
